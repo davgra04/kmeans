69ef4ebe799aaa67ee92f42e4505968635b0dd92
devgru <dgravesdev@gmail.com>
2020-05-19 19:28:25 -0500

    bumped package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kmeans", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="David Graves",
     author_email="dgravesdev@gmail.com",
     description="Baylor College of Medicine Scientific Programmer I Assignment",
